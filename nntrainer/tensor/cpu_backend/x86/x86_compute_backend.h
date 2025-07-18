be0473a668894285abd4fe28f436d4fff7ecb439
Maciej Nalewaj <m.nalewaj@samsung.com>
2025-07-18 17:12:38 +0200

    [Backends] Improvement implementation operations for multi head attention layer
    - Added missing doxygen description
    - Refactoring method compute_fp16vcache_fp32_transposed()
    - Fix and small refactor for AVX2 implementation
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Maciej Nalewaj <m.nalewaj@samsung.com>

--- nntrainer/tensor/cpu_backend/x86/x86_compute_backend.h
+++ nntrainer/tensor/cpu_backend/x86/x86_compute_backend.h
@@ -863,19 +863,64 @@ void quantize_row_q8_0(const T *__restrict src, void *__restrict dst,
  * @param quant_weights additional information for quantization. Currently in no
  * use.
  * @return size_t total size of quantized data
+ * @brief Multihead softmax, exp(x_i) / sum(exp(x_i))
+ * @param[in/out] qk_out float* input/output values
+ * @param[in] start_row start row number
+ * @param[in] end_row end row number
+ * @param[in] num_heads heads number
+ */
+void softmax_row(float *qk_out, size_t start_row, size_t end_row,
+                 size_t num_heads);
+
+/**
+ * @brief Compute vcache for one row transposed
+ * @param[in] row_num row number
+ * @param[in] in float* input vector
+ * @param[in] vcache uint16_t* input vector
+ * @param[out] output float* output vector
+ * @param[in] num_cache_head number head of cache
+ * @param[in] gqa_size size of group
+ * @param[in] head_dim head dimension
+ */
+void compute_fp16vcache_fp32_transposed(int row_num, const float *in,
+                                        const uint16_t *vcache, float *output,
+                                        int num_cache_head, int gqa_size,
+                                        int head_dim);
+
+/**
+ * @brief Compute kcaches
+ * @tparam BType type of B vector element
+ * @param[in] A float* input vector A
+ * @param[in] B BType* input vector B
+ * @param[out] output float* output float vector
+ * @param[in] num_rows number of row
+ * @param[in] N number of chunk
+ * @param[in] chunk_size size of chunk
+ * @param[in] group_size size of group
+ * @param[in] tile_size size of tile
  */
 template <typename T = float>
 size_t quantize_q8_0(const T *src, void *dst, int64_t nrow,
                             int64_t n_per_row, const float *quant_weights);
 /**
- * @brief q8_0 to T dequantize
- *
- * @param x_raw input src to be dequantized
- * @param y output destination for dequantized data
- * @param k data length
+ * @brief Compute rotary embedding value
+ * @param[in] width current w value from b, c, h, w
+ * @param[in] dim unit length of simd computation
+ * @param[in] half_ criterion for rotational direction of embedding
+ * @param[in/out] inout float* uesed also as output when expected output float*
+ * values
+ * @param[out] output void* output values, used when expected output __fp16*
+ * values
+ * @param[in] cos_ float* input con values
+ * @param[in] sin_ float* input sin values
+ * @param[in] only_convert_to_fp16 equal true if method is used only for
+ * conversion
  */
-template <typename T = float>
-void dequantize_row_q8_0(const void *x_raw, T *y, int64_t k);
+void compute_rotary_emb_value(unsigned int width, unsigned int dim,
+                              unsigned int half_, float *inout, void *output,
+                              const float *cos_, const float *sin_,
+                              bool only_convert_to_fp16);
+
 } /* namespace nntrainer */
 #endif /* __cplusplus */
 #endif /* __x86_COMPUTE_BACKEND_H__ */
