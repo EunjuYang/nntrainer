b737849e4c75209f3f01e8484241aa5814714ba3
Eunju Yang <ej.yang@samsung.com>
2026-02-23 14:53:08 +0900

    [CausalLM] Fix undefined variable text_ and TAIL_PROMPT in Windows branch
    The Windows code path used the undeclared identifier text_ (linker error on
    MSVC) where the user prompt should be printed, and referenced the stale
    member TAIL_PROMPT instead of the tail_prompt function parameter. Replace
    both with the matching parameter names to align with the non-Windows branch.
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- Applications/CausalLM/models/causal_lm.h
+++ Applications/CausalLM/models/causal_lm.h
@@ -134,7 +134,6 @@ protected:
 
   unsigned int SYS_PROMP_LEN;
   std::string PRE_COMPUTED_CACHE_PATH;
-  std::string TAIL_PROMPT;
   bool SAVE_KVCACHE;
   bool USE_KVCACHE;
   unsigned int global_token_len;
