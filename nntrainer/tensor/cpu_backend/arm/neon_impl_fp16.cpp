14cf06457bddb60f368dacfe387edf9add299990
Eunju Yang <ej.yang@samsung.com>
2026-03-19 16:16:19 +0900

    [backend] read OMP_NUM_THREADS env var at runtime
    - Previously, OMP_NUM_THREADS was only a compile-time macro (-DOMP_NUM_THREADS=1)
    baked into the binary by the Android build script.
    - Create omp_setting.h in cpu_backend/ as a shared header
    - Add get_runtime_omp_num_threads() and GEMM/GEMV thread helpers
    - Add set_runtime_omp_num_threads() for dynamic override support
    - Remove static caching so OMP_NUM_THREADS env var is read each time
      unless dynamically overridden via set_runtime_omp_num_threads()
    - Update neon_setting.h to be a thin wrapper including omp_setting.h
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- nntrainer/tensor/cpu_backend/arm/neon_impl_fp16.cpp
+++ nntrainer/tensor/cpu_backend/arm/neon_impl_fp16.cpp
@@ -326,10 +326,7 @@ void hgemv(const __fp16 *A, const __fp16 *X, __fp16 *Y, uint32_t M, uint32_t N,
 
 void hgemv_transpose(const __fp16 *A, const __fp16 *X, __fp16 *Y, uint32_t M,
                      uint32_t N, float alpha, float beta) {
-#ifdef OMP_NUM_THREADS
-  set_gemv_num_threads(OMP_NUM_THREADS);
-#endif
-  size_t GEMV_NUM_THREADS = get_gemv_num_threads();
+  size_t GEMV_NUM_THREADS = get_runtime_omp_num_threads();
 
   float *Y32 = new float[N];
   unsigned int idx = 0;
